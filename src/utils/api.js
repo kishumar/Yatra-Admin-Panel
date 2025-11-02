37a8c02cc73e5368fe1a8a9c04cb898d237e844d
kishumar <kishankumar20082000@gmail.com>
2025-11-02 08:24:51 +0530

    SFG

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://yatra-backend-admin.onrender.com/api",
+  baseURL: "https://yatra-backend-admin.onrender.com/api",
   //  headers: { Authorization: `Bearer ${token}` },
   withCredentials: true, 
 
